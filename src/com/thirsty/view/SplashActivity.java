dee77d5fca7555c005836b3f17a3fd7f4242ea92
unknown <mathu@mathu-pc.(none)>
2012-08-18 17:55:03 -0700

    Added timed out transition for Splash screen

--- src/com/thirsty/view/SplashActivity.java
+++ src/com/thirsty/view/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.thirsty.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import com.thirsty.R;
 import com.thirsty.controller.Controller;
@@ -10,14 +12,39 @@ import com.thirsty.controller.Controller;
 public class SplashActivity extends Activity {
 
     private Controller _application;
+    
+    protected boolean _active = true;
+    protected int _splashTime = 5000; // time to display the splash screen in ms
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_view);
         
-         this._application = (Controller)getApplication();
-//         this._application = new Controller();
+        this._application = (Controller)getApplication();
+        
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    _application.nextActivityFromSplashActivity(SplashActivity.this);
+                    _active = false;
+                }
+            }
+        };
+        splashTread.start();
     }
 
     @Override
@@ -26,11 +53,16 @@ public class SplashActivity extends Activity {
         return true;
     }
     
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _active = false;
+        }
+        return true;
+    }
+    
     public void toNextActivity(View view)
     {
     	this._application.nextActivityFromSplashActivity(SplashActivity.this);
-//    	Intent intent = new Intent(SplashActivity.this, SetupActivity.class); 
-//        startActivity(intent); 
-//        finish();
     }
 }
