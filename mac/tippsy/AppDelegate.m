b388eadd8cf7881d86dd14891a45a6420a791444
Amyr <amyr@about.me>
2012-09-26 22:09:58 -0700

    Update app to always exit when button pressed. Also fixed closing connection when navigating away from app.

--- mac/tippsy/AppDelegate.m
+++ mac/tippsy/AppDelegate.m
@@ -40,13 +40,7 @@
 
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    SharedModel *sharedModel = [SharedModel sharedModel];
-    if(sharedModel.listeningForShake == YES)
-    {
-        [[SharedModel sharedModel] stopListeningForShake];
-    }
-        
-    [[SharedModel sharedModel] disconnectSphero];
+
 
 }
 
@@ -56,6 +50,13 @@
 
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    SharedModel *sharedModel = [SharedModel sharedModel];
+    if(sharedModel.listeningForShake == YES)
+    {
+        [[SharedModel sharedModel] stopListeningForShake];
+    }
+    
+    [[SharedModel sharedModel] disconnectSphero];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
